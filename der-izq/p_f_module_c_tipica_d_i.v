a45448658332dee1036e834faa59edb077ae621f
AlejandraSoto <alejandrasa2012@gmail.com>
2023-11-28 11:05:14 -0600

    cambios segun diagrama de la pizarra

--- der-izq/p_f_module_c_tipica_d_i.v
+++ der-izq/p_f_module_c_tipica_d_i.v
@@ -6,10 +6,10 @@ module Celda_tipica_d_i (
     output wire p_x //salida, resultado de la logica combinacional del modulo
 );
 
-wire c1; //conecta la salida de not U1 a la entrada and U2
-wire c2; //salida de U2
-wire c3; //salida de U3
-wire c4; //salida de U4
+wire c1; //salida de N1
+wire c2; //salida de A1
+wire c3; //salida de A2
+wire c4; //salida de A3
 wire c5; //salida de U5
 wire c6; //salida de U6
 
@@ -17,19 +17,19 @@ wire c6; //salida de U6
 //Ecuacion celda tipica ~A&B | x&B | x&~A
 
     // Negar A
-    not U1 (a_p, c1);
+    not N1 (a_p, c1);
 
-    //  ~A con B
-    and U2 (c1, b_p, c2);
-
-    //  x con ~A
-    and U3 (c1, x_p, c3);
+    //  ~A con X
+    and A1 (c1, x_p, c2);
 
     //  x con B
-    and U4 (x_p, b_p, c4);
+    and U3 (b_p, x_p, c3);
+
+    //  ~A con B
+    and A3 (C1, b_p, c4);
 
     // Sumar
-    or U5 (c2, c3, c5);
-    or U6 (c4, c5, c6);
+    or O5 (c2, c3, c5);
+    or O6 (c4, c5, c6);
 
 endmodule
