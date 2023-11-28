5a3e32c30568d15cb7be1e84f25f28a61526c84b
AngelesUJ <127713709+AngelesUJ@users.noreply.github.com>
2023-11-28 09:18:43 -0600

    Cambio 1 del martes 28

--- izq-der/p_f_module_c_final_i_d.v
+++ izq-der/p_f_module_c_final_i_d.v
@@ -1,13 +1,12 @@
-module Celda_final_d_i #(
-    parameter N=3/*preguntar al profe si esta es una definición valida porque no se puede pedir */ 
-    
-) (/*Tiene 4 entradas 1 salida*/
-    input wire a_p, 
+module Celda_final_d_i (
+
+/*Tiene 4 entradas 1 salida*/
+    input wire a_p,
     input wire b_p,
-    input wire x_p, 
+    input wire x_p,
     input wire y_p,
     output wire p_z
 );
-assing p_z = a_p&&~b_p||~y_p&&~b_p||~y_p&&a_p;
-    
+assign p_z = x_p&&y_p||x_p&&a_p&&b_p;
+
 endmodule
